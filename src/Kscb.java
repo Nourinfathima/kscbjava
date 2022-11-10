55d623df6fd34204aeb9954a11bf2103a6723e31
nourin <nourin@NTP-LAP-636.chn.nestgroup.net>
2022-11-11 00:39:41 +0530

    changes

--- src/Kscb.java
+++ src/Kscb.java
@@ -1,9 +1,11 @@
 
 import java.util.Scanner;
-
+import java.sql.*;
 public class Kscb {
     public static void main(String[] args) {
-        int choice;
+        int choice, consumerCode;
+        String consumerName, consumerPhone, consumerEmail, consumerAddress;
+        Scanner input = new Scanner(System.in);
         while (true) {
             System.out.println("select an option");
             System.out.println("1.add consumer");
@@ -20,18 +22,131 @@ public class Kscb {
             switch (choice) {
                 case 1:
                     System.out.println("add consumer");
+                    System.out.println("Add Consumer");
+                    System.out.println("Enter the consumer code: ");
+                    consumerCode = input.nextInt();
+                    System.out.println("Enter Consumer Name: ");
+                    consumerName = input.next();
+                    System.out.println("Enter Consumer Phone: ");
+                    consumerPhone = input.next();
+                    System.out.println("Enter Consumer Email Id: ");
+                    consumerEmail = input.next();
+                    System.out.println("Enter Consumer Address: ");
+                    consumerAddress = input.next();
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksebdb","root","");
+                        String sql = "INSERT INTO `consumer`(`consumerCode`, `consumerName`, `consumerPhone`, `consumerEmail`, `consumerAddress`) VALUES (?,?,?,?,?)";
+                        PreparedStatement stmt = con.prepareStatement(sql);
+                        stmt.setInt(1,consumerCode);
+                        stmt.setString(2,consumerName);
+                        stmt.setString(3,consumerPhone);
+                        stmt.setString(4,consumerEmail);
+                        stmt.setString(5,consumerAddress);
+                        stmt.executeUpdate();
+                        System.out.println("Data inserted successfully.");
+                    }
+                    catch (Exception e ){
+                        System.out.println(e);
+                    }
                     break;
                 case 2:
-                    System.out.println("search consumer");
+                    System.out.println("Search Consumer");
+                    System.out.println("Enter the Consumer Code/Name/Phone to search: ");
+                    String searchOption = input.next();
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksebdb", "root", "");
+                        String sql = "SELECT `consumerCode`, `consumerName`, `consumerPhone`, `consumerEmail`, `consumerAddress` FROM `consumer` WHERE `consumerCode` ='"+searchOption+"'  OR `consumerName`='"+searchOption+"' OR `consumerPhone` ='"+searchOption+"' ";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(sql);
+                        while (rs.next()){
+                            String getConsumerCode = rs.getString("consumerCode");
+                            String getConsumerName = rs.getString("consumerName");
+                            String getConsumerPhone = rs.getString("consumerPhone");
+                            String getConsumerEmail = rs.getString("consumerEmail");
+                            String getConsumerAddress = rs.getString("consumerAddress");
+                            System.out.println("Consumer Code="+getConsumerCode);
+                            System.out.println("Consumer Name="+getConsumerName);
+                            System.out.println("Consumer Phone="+getConsumerPhone);
+                            System.out.println("Consumer Email="+getConsumerEmail);
+                            System.out.println("Consumer Address="+getConsumerAddress);
+                        }
+                    }
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
+
                     break;
                 case 3:
-                    System.out.println("delete consumer");
+                    System.out.println("Delete Consumer");
+                    System.out.println("Enter the consumer code to delete: ");
+                    consumerCode = input.nextInt();
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksebdb", "root", "");
+                        String sql = "DELETE FROM `consumer` WHERE `consumerCode` = "+consumerCode;
+                        Statement stmt = con.createStatement();
+                        stmt.executeUpdate(sql);
+                        System.out.println("Consumer Data deleted successfully.");
+                    }
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
                     break;
                 case 4:
-                    System.out.println("update consumer");
+                    System.out.println("Update Consumer");
+                    System.out.println("Enter the consumer code: ");
+                    consumerCode = input.nextInt();
+                    System.out.println("Enter Consumer Name to update: ");
+                    consumerName = input.next();
+                    System.out.println("Enter Consumer Phone to update: ");
+                    consumerPhone = input.next();
+                    System.out.println("Enter Consumer Email Id to update: ");
+                    consumerEmail = input.next();
+                    System.out.println("Enter Consumer Address to update: ");
+                    consumerAddress = input.next();
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksebdb", "root", "");
+                        String sql = "UPDATE `consumer` SET `consumerName`='"+consumerName+"',`consumerPhone`='"+consumerPhone+"',`consumerEmail`='"+consumerEmail+"',`consumerAddress`='"+consumerAddress+"' WHERE `consumerCode` = "+consumerCode;
+                        Statement stmt = con.createStatement();
+                        stmt.executeUpdate(sql);
+                        System.out.println("Consumer Data updated successfully.");
+                    }
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
                     break;
                 case 5:
-                    System.out.println("update student selected");
+                    System.out.println("View all Consumers");
+                    try{
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ksebdb","root","");
+                        String sql = "SELECT `consumerCode`, `consumerName`, `consumerPhone`, `consumerEmail`, `consumerAddress` FROM `consumer` ";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(sql);
+                        while (rs.next()){
+                            String getConsumerCode = rs.getString("consumerCode");
+                            String getConsumerName = rs.getString("consumerName");
+                            String getConsumerPhone = rs.getString("consumerPhone");
+                            String getConsumerEmail = rs.getString("consumerEmail");
+                            String getConsumerAddress = rs.getString("consumerAddress");
+
+                            System.out.println("Consumer Code="+getConsumerCode);
+                            System.out.println("Consumer Name="+getConsumerName);
+                            System.out.println("Consumer Phone="+getConsumerPhone);
+                            System.out.println("Consumer Email="+getConsumerEmail);
+                            System.out.println("Consumer Address="+getConsumerAddress+"\n");
+
+                        }
+
+                    }
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
+
+
                     break;
                 case 6:
                     System.out.println("genarate consumerbill");
